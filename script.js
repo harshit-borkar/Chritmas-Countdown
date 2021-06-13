3b25e0c9e4c4b85abd6702ca7d92162fc4a7bbfc
Harshit Borkar <85048673+harshit-borkar@users.noreply.github.com>
2021-06-13 15:41:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let daysEl = document.getElementById("days");
+let hoursEl = document.getElementById("hours");
+let minsEl = document.getElementById("mins");
+let secondsEl = document.getElementById("seconds");
+
+const christmas = "25 Dec 2021"
+
+function countdown(){
+    const chritmasDate = new Date(christmas);
+    const currentDate = new Date();
+
+    const totalSeconds = (chritmasDate - currentDate) / 1000;
+    
+    const days = Math.floor(totalSeconds / 60 / 60 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) %60;
+
+    daysEl.innerHTML = formatTime(days);
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time){
+    return time < 10 ? `0${time}` : time;
+}
+
+countdown();
+
+setInterval(countdown,1000);
